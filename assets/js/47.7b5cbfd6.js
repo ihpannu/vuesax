19cbfd30f30eef562b376b0a27a7af01b5c6206b
ldrovira <luisdanielrovira8@gmail.com>
2018-08-04 00:33:52 -0400

    deploy

---
+++ assets/js/47.7b5cbfd6.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[47],{519:function(n,t,e){},630:function(n,t,e){"use strict";var s=e(519);e.n(s).a},851:function(n,t,e){"use strict";e.r(t);var s={},i=(e(630),e(1)),o=Object(i.a)(s,function(){var n=this.$createElement;return(this._self._c||n)("div",{},[this._v("\n  hola soy el demo\n")])},[],!1,null,null,null);t.default=o.exports}}]);
